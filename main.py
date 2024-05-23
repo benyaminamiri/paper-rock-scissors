4413c22a9d8be555fb3af039e88b915639bddc0b
Benyamin Amiri <amiribenyamin1@gmail.com>
2024-05-23 17:59:07 +0330

    computer choice and user choice add

--- main.py
+++ main.py
@@ -2,3 +2,23 @@
 import random
 
 import pics
+
+# choose number between 0 to 2 to simulate rock, paper and scissors
+user_choice = int(input("what do you choose? 0 for Rock, 1 for scissors and 2 for paper:\n"))
+print("Your choice:")
+if user_choice == 0:
+    print(pics.rock)
+elif user_choice == 1:
+    print(pics.scissors)
+elif user_choice == 2:
+    print(pics.paper)   
+
+# use random to choose randomly for computer choice.
+computer_choice = random.randint(0,2)
+print("Computer choice:")
+if computer_choice == 0:
+    print(pics.rock)
+elif computer_choice == 1:
+    print(pics.scissors)
+elif computer_choice == 2:
+    print(pics.paper)    
